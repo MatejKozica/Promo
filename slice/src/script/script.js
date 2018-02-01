d630ff21f263e91fbe207a1406e01553ee0b2298
Matej Kozica <v-matej.kozica@dump.hr>
2018-02-01 10:37:21 +0100

    :hammer:

--- slice/src/script/script.js
+++ slice/src/script/script.js
@@ -1,4 +1,4 @@
-document.getElementsByClassName('icon__hover')[0].addEventListener(
+document.getElementsByClassName('icon')[0].addEventListener(
     'click',
     () => {
         window.open('https://www.youtube.com/');
